fe3ebe37e7d959ce2e26aae9f6b3f2b9cd544b1c
Syed-635 <23pwbcs0998@uetpeshawar.edu.pk>
2025-01-10 21:45:23 +0500

    summury
    thank you

---
+++ server/src/configs/db.js
@@ -0,0 +1,17 @@
+// const mongoose = require('mongoose');
+
+// mongoose.set('strictQuery', true); // Ensuring mongoose uses strictQuery mode
+
+// const connect = () => {
+//     const uri = "mongodb+srv://syedmuhammadshah510:muhammad@cluster0.g207c.mongodb.net/test?retryWrites=true";
+    
+//     return mongoose.connect(uri)
+//         .then(() => {
+//             console.log('Connected to MongoDB Atlas');
+//         })
+//         .catch((error) => {
+//             console.error('Error connecting to MongoDB Atlas:', error);
+//         });
+// };
+
+// module.exports = connect;
